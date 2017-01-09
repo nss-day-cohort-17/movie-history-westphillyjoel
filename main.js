e125dd3bf6521ef201bfda50864469bbbdc4dbbb
Wisbell <westleyisbell@gmail.com>
2017-01-09 17:17:28 -0600

    Found a way to get API data

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // Testing getting JSON
 
-function movieSearchRequest(movieUrl) {
+var storeMovies;
+
+
+function getMovieJSON(movieURL) {
+    return movieSearchRequest.then(JSON.parse)
+}
+
+function requestMovieInfo(url){
     return new Promise (function(resolve, reject){
         var xhr = new XMLHttpRequest()
         xhr.addEventListener('load', function(event){
@@ -11,16 +18,17 @@ function movieSearchRequest(movieUrl) {
             }
         })
         xhr.addEventListener('error', reject)
-        xhr.open('GET', movieUrl)
+        xhr.open('GET', url)
         xhr.send()
     })
-
 }
-
-
-function getMovieJSON(movieURL) {
-    return movieSearchRequest.then(JSON.parse)
-}
-
 
 // Event listener for search button
+
+$('#searchButton').click(function(){
+    var searchQuery = $('#searchText').val()
+    console.log(searchQuery)
+    storeMovie = requestMovieInfo('http://www.omdbapi.com/?s=' + searchQuery)
+        .then(storeMovies = JSON.parse)
+    // storeMovie = getMovieJSON(movieSearchRequest('http://www.omdbapi.com/?s=' + searchQuery))
+})
